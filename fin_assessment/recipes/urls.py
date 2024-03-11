9332e68aa7e1b50ce3108cb4a53b5d8a9edf9927
Renat Prokopiev <prokop4891@gmail.com>
2024-03-11 18:36:30 +0300

    urls add

--- fin_assessment/recipes/urls.py
+++ fin_assessment/recipes/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    path('registration/', views.registration, name='registration'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('add_recipe/', views.add_recipe, name='add_recipe'),
 ]
--- fin_assessment/recipes/views.py
+++ fin_assessment/recipes/views.py
@@ -4,8 +4,30 @@ from random import choice
 
 
 def home(request):
-    home_recipes = []
-    recipes = Recipe.objects.all()
-    for _ in range():
-        home_recipes.append()
-    return render(request, 'recipes/home.html', context={'content': message})
+    test = "HOME TEST"
+    return render(request, 'recipes/home.html', context={'test': test})
+
+
+def add_recipe(request):
+    test = "ADD RECIPE TEST"
+    return render(request, 'recipes/add_recipe.html', context={'test': test})
+
+
+def login(request):
+    test = "LOGIN TEST"
+    return render(request, 'recipes/login.html', context={'test': test})
+
+
+def logout(request):
+    test = "LOGOUT TEST"
+    return render(request, 'recipes/logout.html', context={'test': test})
+
+
+def recipe_detail(request):
+    test = "recipe_detail TEST"
+    return render(request, 'recipes/recipe_detail.html', context={'test': test})
+
+
+def registration(request):
+    test = "REGISTRATION TEST"
+    return render(request, 'recipes/registration.html', context={'test': test})
